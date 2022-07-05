8276681ea567a58b30763432f27a5d27b0a3b818
tamerfs <tamer.srhn@gmail.com>
2022-07-05 14:43:19 -0300

    Update db.ts

--- src/db.ts
+++ src/db.ts
@@ -1,7 +1,7 @@
 // faz a configuração do  banco de dados
 import { Pool } from "pg";
 
-const connectionString = 'postgres://rhzobcea:qOEZR-lk9dPCuyNY4XIzpcb5vkApqlyx@motty.db.elephantsql.com/rhzobcea';
+const connectionString = ''
 
 const db = new Pool ({connectionString});
 
